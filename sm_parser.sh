73516416b5889d8f80ab49a4a353aaaf881710a7
komacat <yeahdsj@gmail.com>
2025-01-28 15:10:14 -0800

    fix local test in shell script

--- sm_parser.sh
+++ sm_parser.sh
@@ -1,9 +1,9 @@
-source .env
 python3 -m venv .venv
 source .venv/bin/activate
+source .env
 pip install -r requirements.txt
 python3 ./sm_parser.py
-if $LOCAL == 'true'; then
+if [ "$LOCAL" = "true" ]; then
     cp songs.json /home/remy/Desktop/proj/test/songs.json
     echo "Songs transferred to local directory"
 else
@@ -11,7 +11,7 @@ else
     echo "Songs transferred to OCF server"
 fi
 python3 ./image_parser.py
-if $LOCAL == 'true'; then
+if [ "$LOCAL" = "true" ]; then
     cp -rt banner /home/remy/Desktop/proj/test/banner
     echo "Banner images transferred to local directory"
 else
