dac519ca2baf6bdd0f00cd99cb44322277a8bdeb
komacat <yeahdsj@gmail.com>
2025-01-29 23:50:46 -0800

    send to aws bucket instead

--- sm_parser.sh
+++ sm_parser.sh
@@ -7,14 +7,16 @@ if [ "$LOCAL" = "true" ]; then
     cp songs.json /home/remy/Desktop/proj/test/songs.json
     echo "Songs transferred to local directory"
 else
-    scp songs.json dgab@apphost.ocf.berkeley.edu:/home/d/dg/dgab/songs.json
-    echo "Songs transferred to OCF server"
+    # scp songs.json dgab@apphost.ocf.berkeley.edu:/home/d/dg/dgab/data/assets/songs.json
+    aws s3 cp songs.json s3://dancegames
+    echo "Songs transferred to AWS S3 bucket"
 fi
 python3 ./image_parser.py
 if [ "$LOCAL" = "true" ]; then
     cp -rt banner /home/remy/Desktop/proj/test/banner
     echo "Banner images transferred to local directory"
 else
-    scp -r banner dgab@apphost.ocf.berkeley.edu:/home/d/dg/dgab/banner
-    echo "Banner images transferred to OCF server"
+    # scp -r banner dgab@apphost.ocf.berkeley.edu:/home/d/dg/dgab/data/assets/banner
+    aws s3 cp banner s3://dancegames --recursive
+    echo "Banner images transferred to AWS S3 bucket"
 fi
